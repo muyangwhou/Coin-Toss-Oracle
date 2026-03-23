6949c09c64bd22822a5e75a1b7363c77f7c678b7
satishpethani92 <satyapethani@gmail.com>
2026-03-23 16:08:30 +0530

    fix the issue

--- server/utils/generateAccessToken.ts
+++ server/utils/generateAccessToken.ts
@@ -1,15 +1,8 @@
-import dotenv from "dotenv";
-
-dotenv.config({ path: "server/.env" });
+require("dotenv").config({ path: "server/.env" });
 import jwt from "jsonwebtoken";
-console.log("process.env.JWT_ACCESS_SECRET_KEY", process.env.JWT_ACCESS_SECRET_KEY)
-console.log("process.env.JWT_ACCESS_EXPIRES_IN", process.env.JWT_ACCESS_EXPIRES_IN)
+
 export const getAccessToken = (payload: { id: string }) => {
-  return jwt.sign(
-    payload,
-    process.env.JWT_ACCESS_SECRET_KEY as string,
-    {
-      expiresIn: process.env.JWT_ACCESS_EXPIRES_IN as string,
-    }
-  );
+  return jwt.sign(payload, process.env.JWT_ACCESS_SECRET_KEY!, {
+    expiresIn: (process.env.JWT_ACCESS_EXPIRES_IN || "1h") as any,
+  });
 };
